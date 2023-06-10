3a877b03877e3cc542783712d1b41f0889811b4b
DACHXY <Danny10132024@gmail.com>
2023-06-10 22:37:38 +0800

    在 dockerfile 中刪除了暫存的 php session 資料

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM dachxy/apache2-php-mssql
 
 EXPOSE 80
 
+# 刪除容器中的所有 php session 暫存資料
+RUN rm -rf /var/lib/php/sessions/*
+
 WORKDIR /var/www/html
 
 COPY . /var/www/html
--- db/dbConnect.php
+++ db/dbConnect.php
@@ -1,5 +1,5 @@
 <?php
-$__DBsevername = "410050042-sql,1433";
+$__DBsevername = "410530042-sql,1433";
 $__DBMaster = "master";
 $__DBname = "ArtworkDB";
 $__DBusername = "sa";
@@ -16,24 +16,24 @@ try {
     $stmt = $pdo->query("SELECT COUNT(*) FROM sys.databases WHERE name = '$__DBname'");
     $count = $stmt->fetchColumn();
 
-    $count = 0;
     // 如果 ArtworkDB 不存在
     if ($count == 0) {
         try {
-            // $pdo->exec("CREATE DATABASE $__DBname");
-            // $pdo->commit();
+            // 創建 ArtworkDB 資料庫
+            $pdo->exec("CREATE DATABASE $__DBname");
             $pdo = null;
 
             $dsn = "sqlsrv:Server=$__DBsevername;Database=$__DBname;TrustServerCertificate=1";
             $pdo = new PDO($dsn, $__DBusername, $__DBpassword);
 
-            // 建立資料庫與資料表
+            // 建立資料表
             $sql = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/scripts/ArtworkDB.sql");
             $result = $pdo->exec($sql);
 
             // 新增資料
             $sql = file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/scripts/insert_data.sql");
             $result = $pdo->exec($sql);
+
         } catch (Exception $e) {
             die("ERROR: " . $e->getMessage());
         }
